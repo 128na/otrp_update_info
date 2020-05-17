01ff3933da6c9e4316d40d3a326e864f0c5736ca
128na <simutrans.128na@gmail.com>
2020-05-17 11:02:11 +0900

    ヘッダーリンクが利かないのを修正

--- resources/js/components/nav-footer.vue
+++ resources/js/components/nav-footer.vue
@@ -2,17 +2,19 @@
   <b-navbar type="dark" variant="secondary">
     <b-container>
       <b-navbar-nav>
-        <span class="mr-1 text-white">Created by</span>
-        <external-link
-          :no_icon="true"
-          class="text-white mr-1"
-          href="https://twitter.com/128na"
-        >@128Na</external-link>
-        <external-link
-          :no_icon="true"
-          class="text-white mr-1"
-          href="https://github.com/128na/otrp_update_info"
-        >GitHub</external-link>
+        <small>
+          <span class="mr-1 text-white">Created by</span>
+          <external-link
+            :no_icon="true"
+            class="text-white mr-1"
+            href="https://twitter.com/128na"
+          >@128Na</external-link>
+          <external-link
+            :no_icon="true"
+            class="text-white mr-1"
+            href="https://github.com/128na/otrp_update_info"
+          >GitHub</external-link>
+        </small>
       </b-navbar-nav>
     </b-container>
   </b-navbar>
